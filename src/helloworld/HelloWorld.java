0b7fd8ae18011dcfabd8db9c9b36ac7e26af7c88
g12951te <g12951te@s30438.tsuda.ac.jp>
2015-04-10 11:33:15 +0900

    さようならを削除しました

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("こんにちは世界");
-        System.out.println("さようなら");
     }
 }
